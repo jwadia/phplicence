f04dca52d3d0de682491d063c74aa9a781aae87e
Jehan W <jehanwadia@bell.net>
2016-08-02 13:28:41 -0400

    Added Secret Key Modal

--- webserver/viewprojects.php
+++ webserver/viewprojects.php
@@ -58,19 +58,42 @@ include 'inc/header.php';
 $results = $con->query("SELECT  name, secret_key FROM projects WHERE username = '" . $username .  "'");
 
 if(mysqli_num_rows($results)>0){
+	$i=0;
 		while($row = $results->fetch_assoc()) {
 			$name = $row['name'];
 			$secret_key = $row['secret_key'];
 			$licences = 0;
-			
+			$i++;
 			echo '<tr>';
 			echo '<th scope="row">' . $name . '</th>';
 			echo '<th>' . $licences . '</th>';
-			echo '<td>' . $secret_key . '</td>';
+			echo '<td> <button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#key'. $i .'">Show</button> </td>';
 			echo '</tr>';
+			echo '
+			<!-- Modal -->
+			<div id="key'. $i .'" class="modal fade" role="dialog">
+			  <div class="modal-dialog">
+
+				<!-- Modal content-->
+				<div class="modal-content">
+				  <div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal">&times;</button>
+					<h4 class="modal-title">Secret Key</h4>
+				  </div>
+				  <div class="modal-body">
+					<p>'. $secret_key .' </p>
+				  </div>
+				  <div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+				  </div>
+				</div>
+
+			  </div>
+			</div> ';
 		}
 		echo '</tbody>';
 		echo '</div></div></div>';
 }
 ?>
 </div>
+</html>
