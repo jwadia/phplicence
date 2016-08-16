0f90c921ebb69c19078076c21c9dcda00411b4fa
Zachary Claret-Scott <github@hcaz.me.uk>
2016-08-16 14:33:11 +0100

    Stoped page from loading with no database

--- webserver/inc/database.php
+++ webserver/inc/database.php
@@ -10,6 +10,7 @@ $con = mysqli_connect($myhost, $myuser, $mypass, $mydb);
 if (mysqli_connect_errno())
 {
 	echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	die; // No point in running past this point with no database.
 }
 
 ?>
